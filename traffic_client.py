09b5b2446e2851ea2561cfa6889e5f807f3955c5
Kobe Kodachi <kodachi@usc.edu>
2023-05-04 22:02:31 -0700

    finished and commented

--- traffic_client.py
+++ traffic_client.py
@@ -1,9 +1,11 @@
 import requests, config
 from typing import Dict
-import datetime
+import datetime, json
+from PIL import Image
+from io import BytesIO
 
 # Inspired by Lab 3's API portion
-SERVER = 'https://localhost:5000'
+SERVER = 'http://172.20.10.3:5000'
 
 # get API url & API key
 URL = "https://router.hereapi.com/v8/routes"
@@ -22,9 +24,6 @@ def get_traffic(start_lat, start_long, dest_lat, dest_long, formatted_date) -> D
     res = requests.get(URL, params=param)
     return res.json()
 
-def seconds_to_minutes():
-    pass
-
 def main():
     # get user input
     dest_lat = input("Enter destination latitude: ")
@@ -32,19 +31,33 @@ def main():
     start_lat = input("Enter starting location latitude: ")
     start_long = input("Enter starting location longitude: ")
 
-    for hr in range(1, 25):
-        formatted_date = date.strftime("%Y-%m-%d")
+    durations = []
+    for hr in range(1, 24):
+        now = datetime.datetime.now()
+        formatted_date = now.strftime("%Y-%m-%d")
         date = datetime.datetime.strptime(formatted_date, "%Y-%m-%d")
 
         date_week_before = date - datetime.timedelta(days=7)
         formatted_date = date_week_before.strftime("%Y-%m-%d")
 
         if (hr <= 9):
-            formatted_date += 'T' + '0' + str(hr) + '00:00-08:00'
+            formatted_date += 'T' + '0' + str(hr) + ':00:00-08:00'
         else:
-            formatted_date += 'T' + str(hr) + '00:00-08:00'
+            formatted_date += 'T' + str(hr) + ':00:00-08:00'
+
         traffic_data = get_traffic(dest_lat, dest_long, start_lat, start_long,formatted_date)
-        response = requests.post(f'{SERVER}/traffic', json=traffic_data)
+
+        # access the "duration" at certain hr
+        curr_duration = traffic_data['routes'][0]['sections'][0]['summary']['duration']
+        durations.append(round(curr_duration / 60, 2))
+
+    response = requests.post(f'{SERVER}/traffic', json=durations)
+
+    # decode the response as a PNG image
+    img = Image.open(BytesIO(response.content))
+
+    # display the image
+    img.show()
 
 if __name__ == "__main__":
     main()
--- traffic_server.py
+++ traffic_server.py
@@ -1,19 +1,51 @@
 from typing import Dict, List, Optional
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import pathlib
-import uuid
 import json
+import plotly.graph_objs as go
 
 app = Flask(__name__)
 # path to directory of this file
 thisdir = pathlib.Path(__file__).parent.absolute() 
 
-# TODO: get all 24 jsons & store in a list
+def load_traffic():
+    try:
+        return json.loads(thisdir.joinpath('traffic_db.json').read_text())
+    except FileNotFoundError:
+        return []
+    
+def seconds_to_minutes():
+    pass
 
-# TODO: parse each json for the "duration" element in JSON
-# convert from seconds to minutes
+@app.route('/traffic', methods=['POST'])
+def traffic():
+    # DONE: get the list of traffic data
+    traffic_data_json = request.get_json()
+    traffic_data = json.dumps(traffic_data_json)
+    traffic_data = json.loads(traffic_data)
+    
+    # DONE: plot element of each hour in a bar graph with plotly
+    # x-axis will be the time (0, 1, 2, ..., 24), y-axis will be minutes
+    times = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', 
+             '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', 
+             '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']
+    trace = go.Bar(x=times, y=traffic_data)
+    layout = go.Layout(
+        title='Road Trip Planner: ',
+        xaxis=dict(title='Time'),
+        yaxis=dict(title='Commute Time (mins)')
+    )
+    fig = go.Figure(data=[trace], layout=layout)
 
-# TODO: plot element of each hour in a bar graph with plotly
-# x-axis will be the time (0, 1, 2, ..., 24), y-axis will be minutes
+    # save img
+    img_bytes = fig.to_image(format='png')
 
-# TODO: save in a png, send back to client
+    # DONE: save in a png, send back to client
+    response = make_response(img_bytes)
+    response.headers.set('Content-Type', 'image/png')
+    response.headers.set('Content-Disposition', 'attachment', filename='roadtrip_planner.png')
+
+    return response
+
+if __name__ == "__main__":
+    app.run(host='172.20.10.3',port=5000, debug=True)
